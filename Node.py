ea8f70908190f5099a4f5d60c8ff3124ce5ef42d
Jstrykow <jakub@strykowski.com>
2022-04-19 20:27:38 +0200

    neighbours not working check add link

--- Node.py
+++ Node.py
@@ -14,11 +14,22 @@ class Node():
 
     def add_link(self, start_node_id: int, end_node_id: int, number_of_fibers: int, fiber_cost: int, number_of_lambdas_in_fiber: int):
         link = Link(start_node_id, end_node_id, number_of_fibers, fiber_cost, number_of_lambdas_in_fiber)
-        self.neighbours.append(end_node_id)
+        self.add_neigbour(start_node_id, end_node_id)
         self.links.append(link)
-  
+
+    def add_link(self, link: Link):
+        link = Link(link.get_start_node_id(), link.get_end_node_id(), link.get_number_of_fibers(), link.get_fiber_cost(), link.get_number_of_fibers())
+        self.add_neigbour(link.get_start_node_id(), link.get_end_node_id())
+        self.links.append(link)
+
     def get_list_of_links(self):
         return self.links
 
+    def add_neigbour(self, start_node_id: int, end_node_id: int):
+        if self.get_id() != end_node_id:
+            self.neighbours.append(end_node_id)
+        else:
+            self.neighbours.append(start_node_id)
+        
     def __str__(self):
         return f"Node ID:{self.get_id()}, neighbours {self.neighbours}\n"
--- input_data_parser.py
+++ input_data_parser.py
@@ -1,3 +1,5 @@
+from cgi import print_arguments
+from operator import truediv
 from graph import Graph
 from link import Link
 from node import Node
@@ -11,18 +13,35 @@ class Input_Reader():
 
     def graph_data(self):
         range = int(self.data[0])
+        nodes = []
         for element in self.data[1:]:
             if(element == '-1\n'):
+                for node in nodes:
+                    print(node)
+                
                 return
             parameters = element.split()
-            # print(element)
             start_node_id = parameters[0]
             end_node_id = parameters[1]
             number_of_fibers = parameters[2]
             fiber_cost = parameters[3]
             number_of_lambdas_in_fiber = parameters[4]
             link = Link(start_node_id, end_node_id, number_of_fibers, fiber_cost, number_of_lambdas_in_fiber)
+            # adding new nodes, checking node is in array
+            if not node_is_in_array(nodes, parameters[0]):
+                node = Node(parameters[0])
+                node.add_link(link)
+                nodes.append(node)
+            if not node_is_in_array(nodes, parameters[1]):
+                node = Node(parameters[1])
+                node.add_link(link)
+                nodes.append(node)
             print(link)
-            
+
+def node_is_in_array(nodes: Node, node_id: int):
+    for node in nodes:
+        if node.get_id() == node_id:
+            return True
+    return False
 
 Input_Reader("data/net4.txt")
