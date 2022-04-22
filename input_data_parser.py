6f19818d5bca99c668da0768d54e0d08a0de02fa
Jstrykow <jakub@strykowski.com>
2022-04-22 17:09:55 +0200

    before demand, route

--- input_data_parser.py
+++ input_data_parser.py
@@ -1,33 +1,25 @@
-from cgi import print_arguments
-from operator import truediv
-from graph import Graph
 from link import Link
 from node import Node
 
-class Input_Reader():
-    def __init__(self, name_of_file):
-        self.name_of_file = name_of_file
+class Input_Reader:
+    def read_links(self, name_of_file):
         with open(name_of_file, "r") as file:
-            self.data = file.readlines()
-        self.links = []
-        self.read_links()
+            range_of_links = int(file.readline())
+            for i in range(range_of_links):
+                parameters = file.readline().split()
+                start_node_id = parameters[0]
+                end_node_id = parameters[1]
+                number_of_fibers = parameters[2]
+                fiber_cost = parameters[3]
+                number_of_lambdas_in_fiber = parameters[4]
+                link = Link(start_node_id, end_node_id, number_of_fibers, fiber_cost, number_of_lambdas_in_fiber)
+                # adding new nodes, checking node is in array
+                # print(link)
+            minus_one = int(file.readline())
+            if(minus_one != -1):
+                print("Error during reading links")
+            file.readline()
 
-    def read_links(self):
-        # not used, maybe fo rerror detection self.range = int(self.data[0]) 
-        for element in self.data[1:]:
-            if(element == '-1\n'):
-                for link in self.links: print(link)
-                return
-            parameters = element.split()
-            start_node_id = parameters[0]
-            end_node_id = parameters[1]
-            number_of_fibers = parameters[2]
-            fiber_cost = parameters[3]
-            number_of_lambdas_in_fiber = parameters[4]
-            link = Link(start_node_id, end_node_id, number_of_fibers, fiber_cost, number_of_lambdas_in_fiber)
-            # adding new nodes, checking node is in array
-            self.links.append(link)
-    
-            
 
-Input_Reader("data/net4.txt")
+ir = Input_Reader()
+ir.read_links("data/net4.txt")
