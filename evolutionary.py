a9dd69db07f7f4df3656256da26d53447f5608df
adamgrabicz <wowdeh-9pycgo-gyHmih>
2022-04-27 19:05:22 +0200

    evo

--- evolutionary.py
+++ evolutionary.py
@@ -1,8 +1,14 @@
 # this file represents implemenation of elvolutionary algorythm
 # it is heuristic : resionable solution in resionable time
-
-from network.Net import Net
+import copy
 import random
+from itertools import product
+from time import time
+from typing import List
+
+from network import Demand
+from network.Chromosome import Chromosome
+from network.Net import Net
 
 """
 Program powinien umożliwiać:
@@ -17,10 +23,11 @@ generacjach),
 
 
 class EvolutionaryAlgorithm:
-    def __init__(self, seed: int, net: Net, number_of_chromosomes: int, max_generations: int, max_time: int, max_generation: int, max_mutation: int, max_no_progress_generation: int, procent_of_best_chromosomes: int, crossover_probability: float, mutation_probability: float):
+    def __init__(self, seed: int, net: Net, prb: str, number_of_chromosomes: int, max_generations: int, max_time: int, max_generation: int, max_mutation: int, max_no_progress_generation: int, procent_of_best_chromosomes: int, crossover_probability: float, mutation_probability: float):
         random.seed(seed)
 
         self.net = net
+        self.prb = prb
         self.number_of_chromosomes = number_of_chromosomes
         self.max_generations = max_generations
         self.max_time = max_time
@@ -31,27 +38,150 @@ class EvolutionaryAlgorithm:
         self.crossover_probability = crossover_probability
         self.mutation_probability = mutation_probability
 
+        self.initial_time = None
         self.generations = 0
         self.no_progress = 0
+        self.results_history =[]
         self.mutation = 0
 
-    def inicjialization_function():
-        pass
-        
+    def calculate(self) -> Chromosome:
+        self.initial_time = time()
+        population = self.get_init_population()
+        solution = Chromosome({})
+
+        while not self.ending():
+            self.generations += 1
+            best_chromosome = Chromosome({})
+
+            # calculate cost function for current generation
+            for chromosome in population:
+                chromosome.calculate(self.net, self.prb)
+                if chromosome.calculate_z(self.net, self.prb) < best_chromosome.z:
+                    best_chromosome = copy.deepcopy(chromosome)
+
+            self.results_history.append(best_chromosome)
+
+            if best_chromosome.z < solution.z:
+                solution = copy.deepcopy(best_chromosome)
+                self.no_progress = 0
+            else:
+                self.no_progress += 1
+
+            population = self.pick_fittest(population)
+
+            # crossover
+            crossed_population = []
+            while len(population) > 1:
+                parents = random.sample(population, 2)
+                population.remove(parents[0])
+                population.remove(parents[1])
+                crossed_population += (self.crossover(parents) if self.crossover_happened() else parents)
+
+            population += crossed_population
+
+            # mutation
+            for chromosome in population:
+                if self.mutation_happened():  # chromosome mutation
+                    for i in range(chromosome.number_of_genes):
+                        if self.mutation_happened():  # gene mutation
+                            chromosome.mutate_gene(i + 1)
+                            self.mutation += 1
+
+        for results in self.results_history:
+            results.calculate_links(self.net)
+
+        solution.calculate_links(self.net)
+
+        return solution
 
     def mutate(seed = None):
         # there is adding radom element in proces of elvolution, it is to move from local optimum. 
         # seed is parametr of function for making possible replication of function
         pass
 
-    def crossover():
-        # exchange genes from two parents, and produce two offspring, better fitting offspring is taking
-        pass
+    def crossover(self, parents):
+        father = parents[0]
+        mother = parents[1]
+        brother = Chromosome({})
+        sister = Chromosome({})
 
-    def fitness():
-        # funtion checking is solution good solution
-        pass
+        number_of_genes = father.number_of_genes
 
-    
-    def best_trajectory():
-        pass
+        for gene_number in range(number_of_genes):
+            if random.random() > 0.5:
+                brother.add_gene(father.get_gene(gene_number + 1))
+                sister.add_gene(mother.get_gene(gene_number + 1))
+            else:
+                brother.add_gene(mother.get_gene(gene_number + 1))
+                sister.add_gene(father.get_gene(gene_number + 1))
+
+        return [brother, sister]
+
+    def pick_fittest(self, population: List):
+        population.sort(key=lambda x: x.z)
+        best_chromosomes = population[:self.number_of_best_chromosomes]
+        padding = [copy.deepcopy(best_chromosomes[i]) for i in range(self.population_padding)]
+        return best_chromosomes + padding
+
+    def crossover_happened(self) -> bool:
+        return random.random() < self.crossover_probability
+
+    def mutation_happened(self) -> bool:
+        return random.random() < self.mutation_probability
+
+    def get_init_population(self) -> List:
+        all_genes_combinations = [self.get_all_possible_chromosomes_with_one_gene(demand) for demand in
+                                  self.net.demands]
+        chromosomes = []
+
+        for i in range(self.number_of_chromosomes):
+            chromosome = Chromosome({})
+            for gene_combination in all_genes_combinations:
+                gene = random.choice(gene_combination).allocation_pattern
+                chromosome.add_gene(gene)
+            chromosome.calculate_links_for_problem(self.net, problem=self.problem)
+            chromosomes.append(chromosome)
+
+        random.shuffle(chromosomes)
+        return chromosomes
+
+    def get_all_possible_chromosomes_with_one_gene(self, demand: Demand) -> List[Chromosome]:
+        volume_split = range(demand.volume + 1)
+        volume_split_for_each_path = [volume_split for _ in range(demand.get_number_of_paths())]
+
+        volume_split_combinations = [combination for combination in product(*volume_split_for_each_path) if
+                                     sum(combination) == demand.volume]
+
+        solutions = [Chromosome(self.build_gene(combination, demand)) for combination in volume_split_combinations]
+        return solutions
+
+    def build_gene(combination: tuple, demand: Demand):
+        allocation_vector = {}
+        for demand_path in demand.demand_paths:
+            path_id = demand_path.path_id
+            flow_xdp = (demand.id, path_id)
+            allocation_vector[flow_xdp] = combination[path_id - 1]
+        return allocation_vector
+
+    def ending(self) -> bool:
+        time_exceeded = time() - self.start_time >= self.max_time
+        if time_exceeded:
+            print("Time limit reached.")
+            return True
+
+        generations_exceeded = self.generation >= self.max_generations
+        if generations_exceeded:
+            print("Generations limit reached.")
+            return True
+
+        mutations_exceeded = self.mutations >= self.max_mutations
+        if mutations_exceeded:
+            print("Mutations limit reached.")
+            return True
+
+        no_progress = self.no_progress >= self.max_no_progress_generations
+        if no_progress:
+            print("Iteration without progress limit reached.")
+            return True
+
+        return False
--- network/Chromosome.py
+++ network/Chromosome.py
@@ -1,7 +1,10 @@
 # chromosome is a feasible solution, it represents sequence of genes, gemes represents internal structure of a solution
-import math
+
 from network.Net import Net
 from network.Path import Path
+import math
+import random
+from pprint import pformat
 
 
 class Chromosome():
@@ -10,13 +13,9 @@ class Chromosome():
         self.link_size = []
         self.link_loads = []
         self.number_of_genes = 0
+        self.z = float('inf')
 
-    def link_load_calculation(self):
-        # link load l(e,x) 
-        pass
-
-    
-    def calculate(self, net: Net):
+    def calculate(self, net: Net, prb: str):
         links = net.links
         link_values = [0] * len(net.links)
         paths = net.get_all_demands_paths()
@@ -26,10 +25,59 @@ class Chromosome():
                 if link_id + 1 in path.links:
                     volume = self.allocation_pattern.get((path.demand_id, path.path_id))
                     volume_sum += volume
-            link_values[link_id] = volume_sum  # DAP
-            # math.ceil(volume_sum / link.modeule) DDAP
+            link_values[link_id] = math.ceil(volume_sum / link.module) if prb == "DDAP" else volume_sum
+        if prb == "DDAP":
+            self.link_size = link_values
+        else:
+            self.link_loads = link_values
+
+    def add_flow(self, mapping: dict):
+        self.allocation_pattern = {**self.allocation_pattern, **mapping}
+
+    def add_gene(self, gene: dict):
+        self.add_flow(gene)
+        self.number_of_genes += 1
+
+    def get_gene(self, demandID):
+        return {key: value for key, value in self.allocation_pattern.items() if key[0] == demandID}
+
+    def mutate(self, gene_number):
+        gene = self.get_gene(gene_number)
+        if len(gene) > 1:
+            flows = random.sample(list(gene), 2)
+            if self.allocation_pattern[flows[0]] > 0:
+                self.allocation_pattern[flows[0]] -= 1
+                self.allocation_pattern[flows[1]] += 1
+
+    def calculate_z(self, net: Net, prob: str):
+        if prob == "DDAP":
+            z = 0
+            for link_id, link_size in enumerate(self.link_sizes):
+                z += net.links[link_id].cost * link_size
+            self.z = z
+        else:
+            z = float('-inf')
+            for i, link_load in enumerate(self.link_loads):
+                _z = link_load - net.links[i].number_of_modules * net.links[i].module
+                if _z > z:
+                    z = _z
+                self.z = z
+        return z
+
+    def calculate_links(self, net: Net):
+        self.calculate(net, "DDAP")
+        self.calculate(net, "DAP")
+
+    def __str__(self):
+        text = "Flows for (demand, path):\n" + pformat(self.allocation_pattern)
+
+        if self.link_sizes:
+            text += f"\nLink sizes: {self.link_sizes}"
+        if self.link_loads:
+            text += f"\nLink loads: {self.link_loads}"
+        text += f"\nz = {self.z}"
+        return text
 
 
-                
 
 
